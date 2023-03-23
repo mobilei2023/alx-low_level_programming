97cc8ddaf8925f57aa43c185737d7a239349a5e0
mobilei <amdanymobilei8@gmail.com>
2023-03-23 05:40:18 -0700

    positive_or_negative.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -3,7 +3,7 @@
 /**
  * positive_or_negative - checks for positive or negative numbers
  *
- *  @i: the number to be checked
+ * 0 : the number to be checked
  *  Return: always 0
  */
 void positive_or_negative(int i);
