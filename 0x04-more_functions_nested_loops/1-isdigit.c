ed219256b5563b37da96736101d215c11c3bbd94
mobilei <amdanymobilei8@gmail.com>
2023-03-24 05:14:08 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,14 +5,12 @@
  *
  * Return: 0 or 1
  */
-
 int _isdigit(int c);
 
 {
-        if (c >= 48  && c <= 57)
+	if (c >= 48 && c <= 57)
 	{
 		return (1);
 	}
-
-	       	return (0);
-}	
+	return (0);
+}		
