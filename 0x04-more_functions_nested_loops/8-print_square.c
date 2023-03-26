384637b3a9fa8c0e2f24bcb3cd4c3f3b55930ef5
mobilei <amdanymobilei8@gmail.com>
2023-03-26 06:10:08 -0700

    vi 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * print_squa -prints a square of size size
+ * print_square -prints a square of size size
  * @size: size of the square
- * Return: void
+ *
+ * Return:void
  */
 
 void print_square(int size)
