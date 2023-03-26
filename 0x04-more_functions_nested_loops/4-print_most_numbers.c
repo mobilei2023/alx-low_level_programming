ea64501457957b3d69ec4c2647b516f4f9826d7a
mobilei <amdanymobilei8@gmail.com>
2023-03-25 23:23:45 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,9 +11,9 @@ void print_most_numbers(void)
 {
 	char c;
 
-	for (c = '0'ci <= '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (!(i == '2' || i == '4'))
+		if (!(c == '2' || c == '4'))
 			_putchar(c);
 	}
 	_putchar('\n');
