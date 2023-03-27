80c479031ea9daa1f22ebe6c69207b293f0b2b35
mobilei <amdanymobilei8@gmail.com>
2023-03-27 03:42:59 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,16 +3,14 @@
 
 /**
  * swap_int - a function that swaps the values of two integers
- * @a: integer to swap 
- * @: integer to swap
+ * @a: integer to swap
+ * @b: integer to swap
  */
 
 void swap_int(int *a, int *b)
-
 {
 	int m;
 	m = *a;
 	*a = *b;
 	*b = m;
-	
 }
