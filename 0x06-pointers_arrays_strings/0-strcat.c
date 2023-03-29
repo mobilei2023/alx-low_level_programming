1031d328365dd29665b3e2f266fa4c8ff2e71b00
mobilei <amdanymobilei8@gmail.com>
2023-03-29 02:04:34 -0700

    0-main.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,22 +4,26 @@
  * _strcat - concatenates two strings
  * @dest: input value
  * @src: input value
- * Return: void 
+ * Return: void
  */
 
 char *_strcat(char *dest, char *src)
-{	
-	int i;
+{
+int i;
 	int j;
+
 	i = 0;
-	while (dest[i] != '0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
-		dest[i] = scr [j];
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
 	dest[i] = '\0';
 	return (dest);
-	}
+}
