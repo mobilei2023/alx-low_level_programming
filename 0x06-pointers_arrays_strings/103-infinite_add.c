6933932018cb9f78307cdd883b3891defa945eaf
mobilei <amdanymobilei8@gmail.com>
2023-03-31 01:21:12 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * infinite_add -adds two numbers.
+ * @size_r: bufer size
+ * Return: pointer to calling function
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp = 0;
+	while (*(n1 +i) != '\0')
+		i++;
+	while(*(n2 + j) !='\0')
+		j++;
+	i--;
+	j--;
+	if (j >= 0 || i > 0 || overflow == 1)
+	{
+		if (i < 0)
+			val2 = 0;
+		else
+			val2 =*(n2 +j) - '0';
+		temp_tot =val1 + val2 + overflow;
+		if  (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >=(size_r -1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
+	}
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
+}
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,7 +13,7 @@ char *leet(char *n);
 char *rot13(char *s);
 void print_number(int n);
 char *rot13(char *);
-
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 
 
