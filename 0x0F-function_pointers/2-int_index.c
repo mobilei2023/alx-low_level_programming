1c9442a67cd8ab2d6f80eb13b02d3026cd4ae553
mobilei <amdanymobilei8@gmail.com>
2023-04-19 05:41:27 -0700

    cs

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,19 +2,20 @@
 /**
  * int_index - searches for an integer
  * @array - array
+ * @size: size of elements in an array
  * @cmp:  a pointer to the function to be used to compare values
  * Return: 0
  *
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+int i;
 	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 	for (i = 0; i < size, i++)
 	{
-		if (cmp(array[i]));
-		return(i);
+		if (cmp(array[i]))
+			return (i);
 	}
 	return (-1);
 }
